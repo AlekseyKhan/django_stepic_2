e1996f6effaa2666de78e8aac544afdafcc78f33
alekha <alekseykhankhatov@gmail.com>
2022-04-18 00:26:12 +0800

    v4 setting, allowedhosts

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&2pg72kg+ffh07#nl-0wwb2=54+e)p-afh(q8kvg$(de9nfi13
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
