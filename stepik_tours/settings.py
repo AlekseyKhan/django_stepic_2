4cbaf57f7d84c5530534723336af93d20d5e25f5
alekha <alekseykhankhatov@gmail.com>
2022-04-18 00:08:18 +0800

    after heroku

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&2pg72kg+ffh07#nl-0wwb2=54+e)p-afh(q8kvg$(de9nfi13
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
